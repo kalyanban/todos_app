eb1b72dc7547be53d23677891837ca525847e2ce
kalyanban <bandarukalyangoud430@gmail.com>
2025-02-09 17:28:10 +0530

    todos_app

--- scripts.js
+++ scripts.js
@@ -2,32 +2,131 @@ const taskInput = document.getElementById("taskInput")
 const addTaskButton = document.getElementById("addButton")
 const tasksList = document.getElementById("todosListContainer")
 
+document.addEventListener("DOMContentLoaded", () => {
+    const storedTasks = JSON.parse(localStorage.getItem("tasks"))
 
-addTaskButton.addEventListener("click", () => {
-    const taskText = taskInput.value.trim()
-    if (taskInput.value === "") return;
-    addTask(taskText)
-    taskInput.value = ""
+    if (storedTasks) {
+        storedTasks.forEach(task => tasks.push(task))
+        updateTasksList()
+        updateStats()
+    }
 })
 
-function addTask(task) {
-    const taskItem = document.createElement("li")
-    taskItem.classList.add("task-item")
+let tasks = []
 
-    taskItem.innerHTML = `
-        <div class="task-text-container">
-            <p>${task}</p>
-            <select>
-                <option>STATUS</option>
-                <option><span><i class="fa-solid fa-circle-exclamation"></i></span> Not Started</option>
-                <option><span><i class="fa-solid fa-clock"></i></span> Pending</option>
-                <option><span><i class="fa-solid fa-check-double"></i></span> Completed</option>
-            </select>
-        </div>
-        <div class="task-item-buttons-container">
-            <button class="edit-button" id="editTask" type="button">Edit</button>
-            <button class="delete-button" id="deleteTask" type="button">Delete</button>
-        </div>
-    `
-    tasksList.appendChild(taskItem)
+const saveTasks = () => {
+    localStorage.setItem("tasks", JSON.stringify(tasks))
+}
+
+const addTask = () => {
+    const taskInputValue = taskInput.value.trim()
+
+    if (taskInputValue) {
+        tasks.push({text: taskInputValue, completed: false})
+        taskInput.value = ""
+
+        updateTasksList()
+        updateStats()
+        saveTasks()
+    }
+}
+
+const toggleTaskComplete = (index) => {
+    tasks[index].completed = !tasks[index].completed
+    console.log({tasks})
+    updateTasksList()
+    updateStats()
+    saveTasks()
+}
+
+const deleteTask = (index) => {
+    tasks.splice(index,1)
+    updateTasksList()
+    updateStats()
+    saveTasks()
+}
+
+const editTask = (index) => {
+    taskInput.value = tasks[index].text
+
+    tasks.splice(index, 1)
+    updateTasksList()
+    updateStats()
+    saveTasks()
+}
+
+const updateStats = () => {
+    const completedTasks = tasks.filter(task => task.completed).length
+    const totalTasks = tasks.length
+    const progress = (completedTasks/totalTasks)*100
+    const progressBar = document.getElementById("status")
+    progressBar.style.width = `${progress}%`
+
+    const stats = document.getElementById("stats")
+    stats.innerText = `${completedTasks} / ${totalTasks}`
+
+    if (tasks.length && completedTasks === totalTasks) {
+        blastConfetti()
+    }
+}
+
+const updateTasksList = () => {
+    tasksList.innerHTML = ''
+
+    tasks.forEach((task, index) => {
+        const li = document.createElement("li")
+
+        li.innerHTML = `
+            <div class="task-item">
+                <div class="task">
+                    <input type="checkbox" class="checkbox" ${task.completed ? "checked" : ""} />
+                    <p class="${task.completed ? "completed" : ""}">${task.text}</p>
+                </div>
+                <div class="icons">
+                    <i class="fa-solid fa-pen-to-square" onClick="editTask(${index})"></i>
+                    <i class="fa-regular fa-trash-can" onClick="deleteTask(${index})"></i>
+                </div>    
+            </div>
+        `
+        li.addEventListener("click", () => toggleTaskComplete(index))
+        tasksList.append(li)
+    })
+}
+
+addTaskButton.addEventListener("click", function(event) {
+    event.preventDefault()
+
+    addTask()
+})
+
+const blastConfetti = () => {
+    const defaults = {
+        spread: 360,
+        ticks: 50,
+        gravity: 0,
+        decay: 0.94,
+        startVelocity: 30,
+        shapes: ["star"],
+        colors: ["FFE400", "FFBD00", "E89400", "FFCA6C", "FDFFB8"],
+      };
+      
+      function shoot() {
+        confetti({
+          ...defaults,
+          particleCount: 40,
+          scalar: 1.2,
+          shapes: ["star"],
+        });
+      
+        confetti({
+          ...defaults,
+          particleCount: 10,
+          scalar: 0.75,
+          shapes: ["circle"],
+        });
+      }
+      
+      setTimeout(shoot, 0);
+      setTimeout(shoot, 100);
+      setTimeout(shoot, 200);
 }
